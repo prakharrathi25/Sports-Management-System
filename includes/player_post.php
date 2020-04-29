732d3c84d4010a29bd57285d0e3cf8753ec3468d
Prakhar Rathi <prakharrathi25@gmail.com>
2020-04-29 15:38:41 +0530

    Changes to the player_reg

--- includes/player_post.php
+++ includes/player_post.php
@@ -1,8 +1,8 @@
 <!-- PHP file for posting the player data to the database -->
 
-error_reporting(-1);
+<!-- error_reporting(-1);
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 1); -->
 
 <?php
     include_once 'dbh.php'
@@ -12,25 +12,54 @@ ini_set('display_startup_errors', 1);
 <?php
 
     // Declare input variables
-    $name = mysqli_real_escape_string($conn, $_POST['name'];
-    $gender = mysqli_real_escape_string($conn, $_POST['gender'];
-    $$match_num = mysqli_real_escape_string($conn, $_POST['$match_num'];
-    $win_num = mysqli_real_escape_string($conn, $_POST['win_num'];
-    $p_sport = mysqli_real_escape_string($conn, $_POST['p_sport'];
-    $s_sport = mysqli_real_escape_string($conn, $_POST['s_sport'];
-    $team = mysqli_real_escape_string($conn, $_POST['team'];
-    $dept = mysqli_real_escape_string($conn, $_POST['dept'];
-    $join_year = mysqli_real_escape_string($conn, $_POST['join_year'];
-    $level = mysqli_real_escape_string($conn, $_POST['level'];
-    $pass_year = mysqli_real_escape_string($conn, $_POST['pass_year'];
-    $quote = mysqli_real_escape_string($conn, $_POST['quote'];
-    $rating = ($match_num/$win_num) * 100;
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $match_num = (int)$_POST['$match_num'];
+    $win_num = (int)$_POST['win_num'];
+    $p_sport = (int)$_POST['p_sport'];
+    $s_sport = (int)$_POST['s_sport'];
+    $team = (int)$_POST['team'];
+    $manager = (int)$_POST['team'];
+    $bid = (int)$_POST['bid'] 
+    $dept = $_POST['dept'];
+    $join_year = (int)$_POST['join_year'];
+    $level = $_POST['level'];
+    $pass_year = (int)$_POST['pass_year'];
+    $quote = $_POST['quote'];
+    // if($match_num > 0 && $win_num > 0){
+    //   $rating = (($win_num * 100)/$match_num);
+    // }
 
+    // Testing the inputs
+    echo "$name" . "<br>";
+    echo "$gender" . "<br>";
+    echo gettype($match_num). "<br>";
+    echo "$win_num" . "<br>";
+    echo "$p_sport" . "<br>";
+    echo "$s_sport" . "<br>";
+    echo "$team" . "<br>";
+    echo "$dept" . "<br>";
+    echo "$join_year" . "<br>";
+    echo "$level" . "<br>";
+    echo "$pass_year" . "<br>";
+    echo "$quote" . "<br>";
+    echo "$rating" . "<br>";
 
-    $sql = "INSERT INTO playerDetails VALUES(NULL, $name, $gender, $match_num, $win_num, NULL, $p_sport, $s_sport, $rating, $team, $team, $bid, $dept, $join_year,  $level, $pass_year, $quote)";
+    $sql = "INSERT INTO playerDetails VALUES($name, $gender, $match_num, $win_num, NULL, $p_sport, $s_sport, NULL, $team, $tseam, $bid, $dept, $join_year,  $level, $pass_year, $quote)";
 
     mysqli_query($conn, $sql)
-
-    header("Location: ../playerReg.html?entry=success")
+    // header("Location: ../playerReg.html?entry=success")
 
 ?>
+
+<div class="jumbotron text-xs-center">
+  <h1 class="display-3">Thank You!</h1>
+  <p class="lead"><strong>Your entry has been stored!</strong></p>
+  <hr>
+  <p>
+    Having trouble? <a href="contact.html">Contact us</a>
+  </p>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="index.html" role="button">Continue to homepage</a>
+  </p>
+</div>
